2dfccbafbab733d9a659c0ffe5e9fd3c52706e53
lucianamartino <lumartino38@gmail.com>
2022-08-31 22:02:08 -0300

    css login

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.listen(3000, ()=> {console.log("Servidor corriendo en el puerto 3000");});
 app.get("/", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/index.html")));
 app.get("/register", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/register.html")));
 app.get("/product", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/product.html")));
-app.get("/login", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/login.html")));
+app.get('/login.html', (req, res)=> res.sendFile(path.resolve(__dirname, "./views/login.html")));
 app.get("/cart", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/cart.html")));
 
