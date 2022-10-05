21ff13252499258056b4d53864b3d3e309e5a25b
YagoR02 <ytromano2@gmail.com>
2022-10-04 23:28:34 -0300

    carpeta src

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const nosotrosRouter = require('./src/routes/nosotros.routes');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
