0c2fe811f2357669d8165901ce82b6254dbb43d5
YagoR02 <ytromano2@gmail.com>
2022-10-05 22:59:43 -0300

    crud

--- SRC/controllers/products.controller.js
+++ SRC/controllers/products.controller.js
@@ -1,4 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+
+function findAll(){
+    const jsonData = fs.readFileSync(path.join(__dirname, "../data/products.json"));
+    const data = JSON.parse(jsonData);
+    return data
+}
+
+function writeFile(data){
+    const dataString = JSON.stringify(data, null, 4);
+    fs.writeFileSync(path.join(__dirname, "../data/products.json"), dataString); 
+}
+
+
+
 const productController = {
+    list: (req, res) =>{
+        const data = findAll()
+        res.render("menu-products", { products: data})  
+    },
+    detail: (req,res) =>{
+        const data = findAll()
+        const platoEncontrado = data.find(function(plato){
+            return plato.id == req.params.id   
+        })
+
+        res.render("product-detail", { plato : platoEncontrado})
+    },
+    create: (req,res) =>{
+        res.render("product-create-form");
+    },
+    store: (req,res) =>{
+        const data = findAll()  
+
+        const newProduct = {   
+            id: data.length + 1,
+            name: req.body.name,
+            price: Number(req.body.price),
+            category: req.body.category,
+            description: req.body.description,
+            image: req.file.filename
+        }
+
+        data.push(newProduct);  
+
+        writeFile(data)  
+
+        res.redirect("/products/list"); 
+    },
 
 
 // Render vista de product
--- SRC/routes/products.routes.js
+++ SRC/routes/products.routes.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller')
 const router = express.Router();
+const multer = require('multer');
+const path = require("path");
+
+let storage = multer.diskStorage({
+	destination: (req, file, cb) => { 
+        cb(null, path.join(__dirname,'../../public/images' ))
+	},
+
+	filename: (req, file, cb) => { 
+        const newFilename = file.fieldname + Date.now() + "-" + path.extname(file.originalname);
+		cb(null, newFilename);
+    }
+})  
+
+let upload = multer({storage: storage});
+
+
+//Crud
+router.get("/list", productsController.list);
+router.get("/detail/:id", productsController.detail);
+router.get("/create", productsController.create);
+router.post("/create", upload.single("image"), productsController.store);
 
 // Get products page
 
