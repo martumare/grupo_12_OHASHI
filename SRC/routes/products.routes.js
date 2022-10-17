153e899bbea437ccdd403feec8af9088e5dfaea5
martumare <maremartinica@gmail.com>
2022-10-16 22:35:31 -0300

    Cambios

--- SRC/routes/products.routes.js
+++ SRC/routes/products.routes.js
@@ -2,23 +2,24 @@ const express = require('express');
 const productsController = require('../controllers/products.controller')
 const router = express.Router();
 const multer = require('multer');
-const path = require("path");
+const path = require('path');
 
-let storage = multer.diskStorage({
+// Configuracion MULTER
+const storage = multer.diskStorage({
 	destination: (req, file, cb) => { 
-        cb(null, path.join(__dirname,'../../public/images' ))
+        cb(null, path.join(__dirname,'../../public/images/imagenes-platos'))
 	},
 
 	filename: (req, file, cb) => { 
         const newFilename = file.fieldname + Date.now() + "-" + path.extname(file.originalname);
 		cb(null, newFilename);
     }
-})  
+});  
 
-let upload = multer({storage: storage});
+const upload = multer( {storage: storage} );
 
 
-//Crud
+// CRUD
 router.get("/", productsController.menu);
 router.get("/detail/:id", productsController.detail);
 
@@ -29,7 +30,6 @@ router.get("/edit/:id", productsController.edit);
 router.put("/edit/:id", productsController.update);
 
 router.delete("/delete/:id", productsController.delete);
-
 
 
 // Get carrito page
@@ -43,5 +43,7 @@ router.get('/reservar', productsController.reservar);
 // Get pedir page
 
 router.get('/pedir', productsController.pedir);
+
+
 
 module.exports = router;
